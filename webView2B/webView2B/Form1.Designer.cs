9683209b93c89463b6d9ad807e8e750e45ae0438
Kttra <100814612+Kttra@users.noreply.github.com>
2022-03-23 16:34:17 -0700

    Added Project

---
+++ webView2B/webView2B/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace webView2B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.addressBar = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(1, 47);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(802, 405);
+            this.webView.Source = new System.Uri("https://www.bing.com", System.UriKind.Absolute);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // addressBar
+            // 
+            this.addressBar.Location = new System.Drawing.Point(1, 0);
+            this.addressBar.Name = "addressBar";
+            this.addressBar.Size = new System.Drawing.Size(735, 20);
+            this.addressBar.TabIndex = 1;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(742, 0);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(52, 20);
+            this.goButton.TabIndex = 2;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.addressBar);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.TextBox addressBar;
+        private System.Windows.Forms.Button goButton;
+    }
+}
+
---
+++ webView2B/webView2B/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace webView2B
+{
+   public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            webView.NavigationStarting += EnsureHttps;
+        }
+        //Ensure that the address starts with "https://"
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                webView.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                args.Cancel = true;
+            }
+        }
+        //Resize address bar and go button
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new System.Drawing.Size(webView.Location);
+            goButton.Left = this.ClientSize.Width - goButton.Width;
+            addressBar.Width = goButton.Left - addressBar.Left;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {   
+            //Checks to see if link is valid
+            string address = addressBar.Text;
+            if (address.Length < 8 || address.Substring(0, 8) != "https://" || addressBar.Text == "https://")
+            {
+                webView.CoreWebView2.ExecuteScriptAsync($"alert('Link is not valid, remember to start with https://')");
+            }
+            //Link is almost valid enough
+            else
+            {
+                try
+                {
+                    webView.CoreWebView2.Navigate(addressBar.Text);
+                }
+                //Catch any other errors we might not have accounted for
+                catch(Exception error)
+                {
+                    webView.CoreWebView2.ExecuteScriptAsync($"Not a valid link");
+                }
+            }
+        }
+
+    }
+}
