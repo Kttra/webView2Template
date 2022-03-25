6ea0b17424a71b2e91b16ecec6417f2a5336e209
Kttra <100814612+Kttra@users.noreply.github.com>
2022-03-24 18:17:22 -0700

    Update progressB Class File

--- json/progressB.cs
+++ json/progressB.cs
@@ -1,8 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+/*
+    progressB class file
+*/
 
 namespace webView2B
 {
@@ -12,5 +16,18 @@ namespace webView2B
         public int maximum;
         public int minimum;
         public int value;
+
+    //Create class with no entries: progressB newEntry = new progressB();
+    public progressB()
+    {
+
+    }
+    //Create class with entries: progressB newEntry = new progressB(10, 10, 10);
+    public progressB(int aEdge, int aDesktop, int aMobile)
+    {
+        edge = aEdge;
+        desktop = aDesktop;
+        mobile = aMobile;
+    }
     }
 }
