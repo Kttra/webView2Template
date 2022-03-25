834ea3b69f6f2950b5d788b44cebc979466d8850
Kttra <100814612+Kttra@users.noreply.github.com>
2022-03-24 17:50:02 -0700

    Add files via upload

---
+++ json/progressB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webView2B
+{
+    [Serializable]
+    class progressB
+    {
+        public int maximum;
+        public int minimum;
+        public int value;
+    }
+}
